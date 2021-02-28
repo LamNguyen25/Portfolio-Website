eb69f126bfb7f212263006e6d340573e812db5fb
Lam Nguyen <lamnguyen3801@gmail.com>
2021-02-28 13:12:05 -0800

    add locomotive

--- src/App.js
+++ src/App.js
@@ -2,9 +2,12 @@
 import React, { useState, useEffect } from 'react';
 import './assets/css/style.css';
 import { Typography, Avatar, Grid,makeStyles, Card, CardActions, CardContent } from '@material-ui/core';
-import locomotiveScroll from "locomotive-scroll";
+import LocomotiveScroll from "locomotive-scroll";
 import ChevronRightIcon from '@material-ui/icons/ChevronRight';
 import PlayArrowIcon from '@material-ui/icons/PlayArrow';
+import './assets/css/locomotive-scroll.css';
+import './assets/css/locomotive-scroll.min.css';
+
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -22,22 +25,95 @@ function App() {
   const classes = useStyles();
 
   const [jobNumber, setJobNumber] = useState();
+  const [selectedJob, setSelectedJob] = useState();
 
   useEffect(() => {
-
     if(!jobNumber) {
-      setJobNumber(0);
+      setJobNumber(1);
+      setSelectedJob(displayJob1);
+    } else {
+      if(jobNumber == 1) {
+        setSelectedJob(displayJob1);
+      } else {
+        setSelectedJob(displayJob2);
+      }
     }
 
-    // const scroll = new locomotiveScroll({
-    //   el: scrollRef.current,
-    //   smooth: true
-    // });
-  });
+    const scroll = new LocomotiveScroll({
+      // el: document.querySelector(".App"),
+      smooth: true
+    });
+    console.log(jobNumber);
+    
+  },[jobNumber]);
+
+  var displayJob1 = (() => {
+    return (
+      <Grid item xs container direction="column" spacing={2} alignItems="flex-start">
+      <Grid item xs>
+        <Typography variant="h5" component="h2" align="left">
+          Software engineer Intern 
+          <span style={{color: '#2a9d8f'}}> @ Skipli</span>
+        </Typography>
+        <Typography variant="subtitle2" align="left">
+          January 2021 - Present
+        </Typography>
+      </Grid>
+      <Grid item xs>
+        <div className="display-row">
+          <PlayArrowIcon style={{ color: '#2a9d8f', marginRight: '10px' }}/>
+          <Typography variant="subtitle1" align="left">Design & create a mobile application for the restaurant’s guests to download on their smartphone (Android & iOS) using React Native, Firebase Real-time Database (DB), and NodeJS</Typography>
+        </div>
+        
+        <div className="display-row">
+          <PlayArrowIcon style={{ color: '#2a9d8f', marginRight: '10px' }}/>
+          <Typography variant="subtitle1" align="left">Allow guests to collect loyalty points from each purchase and place orders through Skipli Checkout</Typography>
+        </div>
+
+        <div className="display-row">
+          <PlayArrowIcon style={{ color: '#2a9d8f', marginRight: '10px' }}/>
+          <Typography variant="subtitle1" align="left">Utilize Facebook & IG API to show guests the restaurant’s Facebook and Instagram posts</Typography>
+        </div>
+      </Grid>
+    </Grid>
+    )
+  })
+
+  var displayJob2 = (() => {
+    return (
+      <Grid item xs container direction="column" spacing={2} alignItems="flex-start">
+      <Grid item xs>
+        <Typography variant="h5" component="h2" align="left">
+          Full Stack Web Developer Intern 
+          <span style={{color: '#2a9d8f'}}> @ VNPN</span>
+        </Typography>
+        <Typography variant="subtitle2" align="left">
+          Sep. 2020 – Dec. 2020
+        </Typography>
+      </Grid>
+      <Grid item xs>
+        <div className="display-row">
+          <PlayArrowIcon style={{ color: '#2a9d8f', marginRight: '10px' }}/>
+          <Typography variant="subtitle1" align="left">Developed, modified, and tested login and other features using OAuth2 flow with backend written in NodeJS, and single-page-application registration process using MVC model, served over 10 million visits since 2018, successfully created 2,000 mentorship sessions since 2018</Typography>
+        </div>
+        
+        <div className="display-row">
+          <PlayArrowIcon style={{ color: '#2a9d8f', marginRight: '10px' }}/>
+          <Typography variant="subtitle1" align="left">Utilized HTML, CSS, and JavaScript to update the web system/responsive layouts, improve user experience, and add new functionalities while making use of NoSQL databases (MongoDB)</Typography>
+        </div>
+
+        <div className="display-row">
+          <PlayArrowIcon style={{ color: '#2a9d8f', marginRight: '10px' }}/>
+          <Typography variant="subtitle1" align="left">Maintained frequent communication and high productivity while working remotely</Typography>
+        </div>
+      </Grid>
+    </Grid>
+    )
+  })
 
   return (
-    <div className="container-fluid p-0 m-0">
-      <section id="home" className="wrapper">
+    <div className="container-fluid p-0 m-0" data-scroll-container>
+      <section id="home" className="wrapper" data-scroll-section>
         <nav id="navbar">
             <ul>
                 <li><a href="#home">Home</a></li>
@@ -55,7 +131,7 @@ function App() {
                   </span>
                   
               </div>
-              <div className="col-6">
+              <div className="col-6" data-scroll>
                   <div className="title-content">
                       <div className="title-content__container">
                           <p className="title-content__container__text">
@@ -84,20 +160,18 @@ function App() {
             </div>
       </section>
 
-      <section id="about" className="wrapper sec-1">
+      <section id="about" className="wrapper sec-1" data-scroll-section>
           <div className="title">About Me</div>
-          <div className={classes.root}>
+          <div className={classes.root }>
             <Grid container spacing={2}> 
-              <Grid item xs={6}>
-                <div className="introduction">
-                  <Typography variant="h7">
+              <Grid item xs={6} style={{paddingLeft: '60px'}} >
+                  <Typography variant="subtitle1" align="right" data-scroll data-scroll-direction="vertical" data-scroll-position="bottom" data-scroll-speed="2" data-scroll-sticky>
                       Hello! I'm Lam, a software engineer based in Los Angeles, CA. I am a fast learner with a strong work ethic in a team or individual setting to drive product success and process efficiency
                   </Typography>
-                </div>
-                
               </Grid>
-              <Grid item xs={6}>
-                <p>Here are a few technologies I've been working with recently:</p>
+              <Grid item xs={6} data-scroll data-scroll-direction="vertical" data-scroll-position="right" data-scroll-speed="2" >
+              <Typography variant="subtitle1" align="left" 
+              >Here are a few technologies I've been working with recently:</Typography>
                 <ul className="skill-list">
                   <li><PlayArrowIcon style={{ color: '#64ffda' }}/> JavaScript</li>
                   <li><PlayArrowIcon style={{ color: '#64ffda' }}/> Java</li>
@@ -110,85 +184,38 @@ function App() {
                   <li><PlayArrowIcon style={{ color: '#64ffda' }}/> jQuery</li>
                   <li><PlayArrowIcon style={{ color: '#64ffda' }}/> Git</li>
                 </ul>
-                {/* <Grid container spacing={2}> 
-                  <Grid item xs={6}>
-                    <li><PlayArrowIcon style={{ color: '#64ffda' }}/> JavaScript</li>
-                    <li><PlayArrowIcon style={{ color: '#64ffda' }}/> Java</li>
-                    <li><PlayArrowIcon style={{ color: '#64ffda' }}/> Node.js</li>
-                    <li><PlayArrowIcon style={{ color: '#64ffda' }}/> MongoDB</li>
-                    <li><PlayArrowIcon style={{ color: '#64ffda' }}/> Cloud Firestore</li>
-                  </Grid>
-                  <Grid item xs={6}>
-                    <ul>
-                      <li><PlayArrowIcon style={{ color: '#64ffda' }}/> HTML & (S)CSS</li>
-                      <li><PlayArrowIcon style={{ color: '#64ffda' }}/> ReactJS</li>
-                      <li><PlayArrowIcon style={{ color: '#64ffda' }}/> React Native</li>
-                      <li><PlayArrowIcon style={{ color: '#64ffda' }}/> jQuery</li>
-                      <li><PlayArrowIcon style={{ color: '#64ffda' }}/> Git</li>
-                    </ul>
-                  </Grid>
-                </Grid> */}
               </Grid>
             </Grid>
           </div>
       </section>
 
-      <section id="experience" className="wrapper sec-2">
+      <section id="experience" className="wrapper sec-2" data-scroll-section>
         <div className="title">Where I've Worked</div>
         <Grid container spacing={2}> 
           <Grid item xs={3}>
-            <ul>
+            <ul id="job">
               <li>
-                <button className="selected-job-btn" style={{marginBottom: '10px'}}>Skipli</button>
+                <button 
+                  className="selected-job-btn" 
+                  style={{marginBottom: '10px'}} 
+                  onClick={()=>{setJobNumber(1)}}
+                >
+                  Skipli
+                </button>
               </li>
               <li>
-                <button className="selected-job-btn">VNPN</button>
+                <button 
+                  className="selected-job-btn"
+                  onClick={()=>{setJobNumber(2)}}
+                >
+                  VNPN
+                </button>
               </li>
             </ul>
           </Grid>
-          {/* <Grid item xs={6}>
-            <Typography variant="h5" component="h2" style={{marginLeft: '0px'}}>
-              Software engineer Intern 
-              <span style={{color: '#2a9d8f'}}> @ Skipli</span>
-            </Typography>
-            <Typography variant="subtitle2" style={{marginLeft: '0px'}}>
-                January 2021 - Present
-            </Typography>
-            <ul>
-              <li><PlayArrowIcon style={{ color: '#64ffda' }}/> JavaScript</li>
-              <li><PlayArrowIcon style={{ color: '#64ffda' }}/> Java</li>
-            </u>
-          </Grid>
-          <Grid item xs={3}></Grid> */}
-          <Grid item xs={6} sm container>
-            <Grid item xs container direction="column" spacing={2} alignItems="flex-start">
-              <Grid item xs>
-                <Typography variant="h5" component="h2" style={{marginLeft: '40px'}}>
-                  Software engineer Intern 
-                  <span style={{color: '#2a9d8f'}}> @ Skipli</span>
-                </Typography>
-                <Typography variant="subtitle2" style={{marginLeft: '0px'}}>
-                  January 2021 - Present
-                </Typography>
-              </Grid>
-              <Grid item justify="flex-start">
-                <div className="display-row">
-                  <PlayArrowIcon style={{ color: '#64ffda' }}/>
-                  <p className="jobDescription">Design & create a mobile application for the restaurant’s guests to download on their smartphone (Android & iOS) using React Native, Firebase Real-time Database (DB), and NodeJS</p>
-                </div>
-                
-                <div className="display-row">
-                  <PlayArrowIcon style={{ color: '#64ffda' }}/>
-                  <p className="jobDescription">Allow guests to collect loyalty points from each purchase and place orders through Skipli Checkout</p>
-                </div>
 
-                <div className="display-row">
-                  <PlayArrowIcon style={{ color: '#64ffda' }}/>
-                  <p className="jobDescription">Utilize Facebook & IG API to show guests the restaurant’s Facebook and Instagram posts</p>
-                </div>
-
-              </Grid>
-            </Grid>
+          <Grid item xs={6} sm container data-scroll-direction="vertical" data-scroll-speed="2">
+            {selectedJob}
           </Grid>
           <Grid item xs={3}></Grid> 
         </Grid>
